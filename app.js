d5825e99c75744c20dab44aa0a09268ee7ee597e
nasim0994 <nasimuddin0994@gmail.com>
2022-09-20 20:23:04 +0600

    updatesite

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 var a;
-function showHide(){
-    if(a==1){
-        document.querySelector('.headerManu').style.display = "block";
-        return a=0;
-    }
-    else{
-        document.querySelector('.headerManu').style.display = "none";
-        return a=1;
-    }
+function showHide() {
+  if (a == 1) {
+    document.querySelector(".headerManu").style.display = "block";
+    return (a = 0);
+  } else {
+    document.querySelector(".headerManu").style.display = "none";
+    return (a = 1);
+  }
 }
+
+document.getElementById("contact-btn").addEventListener("click", function () {
+  const name = document.getElementById("name-filed");
+  const email = document.getElementById("email-filed");
+  const subject = document.getElementById("subject-filed");
+  const message = document.getElementById("message-filed");
+  //Name:
+  if (name.value) {
+    name.value = "";
+  } else {
+    alert("Please Enter your Name");
+  }
+  //Email:
+  if (email.value) {
+    email.value = "";
+  } else {
+    alert("Please Enter your Email Address");
+  }
+  //Subject:
+  if (subject.value) {
+    subject.value = "";
+  } else {
+    alert("Please Enter your subject");
+  }
+  //Message:
+  if (message.value) {
+    message.value = "";
+  } else {
+    alert("Please Enter your Message");
+  }
+});
